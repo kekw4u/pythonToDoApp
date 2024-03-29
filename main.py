1ab78e718f5f6c0b1810bd7bc5b1fad02a02248f
kekw4u <dmitriiii95@mail.ru>
2024-03-29 23:08:51 +0300

    Добавил функцию добавления тудушки

--- main.py
+++ main.py
@@ -17,3 +17,27 @@ def create_todo_list(path_todo, todo_name):
             sort_keys=True,
             indent=4,
         )
+
+
+def add_todo(path_todo, new_todo):
+    with open(path_todo, 'r') as todo_file:
+        data = json.load(todo_file)
+
+    name = data["name"]
+    todos = data["todos"]
+
+    todos.append(new_todo)
+
+    new_data = {
+        "name": name,
+        "todos": todos,
+    }
+
+    with open(path_todo, "w", encoding='utf-8') as todo_file:
+        json.dump(
+            new_data,
+            todo_file,
+            sort_keys=True,
+            indent=4,
+            ensure_ascii=False,
+        )
