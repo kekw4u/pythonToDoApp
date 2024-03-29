ba1838e10da56b4bff2f74f4c45585b0c8853854
kekw4u <dmitriiii95@mail.ru>
2024-03-30 00:26:32 +0300

    Закомментировал код

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+"""                 Приложение ToDoList
+
+Todo:
+    * Сюда пишем то, что надо сделать в последующем
+
+"""
+
 import json
-import sys
 
 
-def main():
-    pass
+def main() -> None:
+    """
+    Returns:
+        None: Ничего не возвращаем
+    """
+    print("Hello, World!")
 
 
 if __name__ == '__main__':
@@ -11,11 +21,28 @@ if __name__ == '__main__':
 
 
 class TodoJournal:
-    def __init__(self, path_todo, name):
+    """
+    Класс ToDoListJournal
+
+    Attributes:
+        path_todo(str): Путь до файла с туду листом
+        name(str): Название туду листа
+    """
+
+    def __init__(self, path_todo: str, name: str):
+        """Конструктор класса: создаёт объект с заданными аттрибутами
+
+        Args:
+            path_todo(str): Путь до файла с туду листом
+            name(str): Название туду листа
+
+        Returns:
+        """
         self.path_todo = path_todo
         self.name = name
 
-    def create(self):
+    def create(self) -> None:
+        """Создаёт файл с заданными json-параметрами"""
         with open(self.path_todo, "w", encoding='utf-8') as todo_file:
             json.dump(
                 {"name": self.name, "todos": []},
@@ -25,7 +52,15 @@ class TodoJournal:
                 ensure_ascii=False,
             )
 
-    def add_entry(self, new_entry):
+    def add_entry(self, new_entry) -> None:
+        """Добавляет новую тудушку в существующий туду лист
+
+        Args:
+            new_entry(str): Туду, который будет добавлен
+
+        Returns:
+
+        """
         data = self._parse()
 
         name = data["name"]
@@ -40,7 +75,15 @@ class TodoJournal:
 
         self._update(new_data)
 
-    def remove_entry(self, index):
+    def remove_entry(self, index: int) -> None:
+        """Удаляет тудушку с заданным индексом
+
+        Args:
+            index(int): Номер тудушки, которая будет удалена
+
+        Returns:
+
+        """
         data = self._parse()
         name = data["name"]
         todos = data["todos"]
@@ -54,7 +97,15 @@ class TodoJournal:
 
         self._update(new_data)
 
-    def _update(self, new_data):
+    def _update(self, new_data: dict) -> None:
+        """Обновляет туду лист, записывая туда данные из словаря new_data
+
+        Args:
+            new_data(dict): Список Тудушек
+
+        Returns:
+
+        """
         with open(self.path_todo, "w", encoding='utf-8') as todo_file:
             json.dump(
                 new_data,
@@ -64,7 +115,12 @@ class TodoJournal:
                 ensure_ascii=False,
             )
 
-    def _parse(self):
-        with open(self.path_todo, 'r') as todo_file:
+    def _parse(self) -> dict:
+        """Получает данные из туду листа
+
+        Returns:
+            dict: Данные, полученные из файла(Туду листа)
+        """
+        with open(self.path_todo, 'r', encoding='utf-8') as todo_file:
             data = json.load(todo_file)
         return data
