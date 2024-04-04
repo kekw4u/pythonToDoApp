7820e590ec4b423a01e7f64378d69bdf570a513c
kekw4u <dmitriiii95@mail.ru>
2024-04-04 22:15:55 +0300

    Протестировал функцию добавления записи в туду журнал

--- tests/test_todo_journal.py
+++ tests/test_todo_journal.py
@@ -11,6 +11,7 @@ def test_init() -> None:
     Функция тестирования TodoJournal'а
     """
     test_create_journal("data/")
+    test_add_entry("data/")
 
 
 def test_create_journal(tmpdir):
@@ -25,6 +26,26 @@ def test_create_journal(tmpdir):
         indent=4)
 
     TodoJournal.create(todo, "test")
+    with open(todo, 'r', encoding='utf-8') as todoFile:
+        data = todoFile.read()
+    assert expected_todo == data
+
+
+def test_add_entry(tmpdir):
+    todo_filename = "test_todo"
+    todo = tmpdir+todo_filename
+
+    expected_todo = json.dumps(
+        {
+        "name": "test",
+        "todos": ["Сходить за молоком"]
+        },
+        indent=4,
+        ensure_ascii=False,)
+
+    TodoJournal.create(todo, "test")
+    todo_jrnl = TodoJournal(todo)
+    todo_jrnl.add_entry("Сходить за молоком")
     with open(todo, 'r', encoding='utf-8') as todoFile:
         data = todoFile.read()
     assert expected_todo == data
