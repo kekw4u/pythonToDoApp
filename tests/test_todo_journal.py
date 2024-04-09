db095c10e3a28c6c797bcf5e16333f78da535775
kekw4u <dmitriiii95@mail.ru>
2024-04-09 19:50:35 +0300

    Добавил тесты для метода __len__

--- tests/test_todo_journal.py
+++ tests/test_todo_journal.py
@@ -9,13 +9,11 @@ def test_init() -> None:
     """
     Функция тестирования TodoJournal'а
     """
-    test_create_journal("data/")
-    test_add_entry("data/")
 
 
-def test_create_journal(tmpdir):
+def test_create_journal_validpath(tmpdir):
     todo_filename = "test_todo"
-    todo = tmpdir+todo_filename
+    todo = tmpdir + todo_filename
 
     expected_todo = json.dumps(
         {
@@ -32,15 +30,15 @@ def test_create_journal(tmpdir):
 
 def test_add_entry(tmpdir):
     todo_filename = "test_todo"
-    todo = tmpdir+todo_filename
+    todo = tmpdir + todo_filename
 
     expected_todo = json.dumps(
         {
-        "name": "test",
-        "todos": ["Сходить за молоком"]
+            "name": "test",
+            "todos": ["Сходить за молоком"]
         },
         indent=4,
-        ensure_ascii=False,)
+        ensure_ascii=False, )
 
     TodoJournal.create(todo, "test")
     todo_jrnl = TodoJournal(todo)
@@ -48,3 +46,27 @@ def test_add_entry(tmpdir):
     with open(todo, 'r', encoding='utf-8') as todoFile:
         data = todoFile.read()
     assert expected_todo == data
+
+
+def test_len_method_empty_journal(tmpdir):
+    todo_filename = "test_todo"
+    todo = tmpdir + todo_filename
+
+    expected_len = 0
+    TodoJournal.create(todo, "test")
+    todo_jrnl = TodoJournal(todo)
+    count_of_entries = len(todo_jrnl)
+    assert expected_len == count_of_entries
+
+
+def test_len_method_two_entries_journal(tmpdir):
+    todo_filename = "test_todo"
+    todo = tmpdir + todo_filename
+
+    expected_len = 2
+    TodoJournal.create(todo, "test")
+    todo_jrnl = TodoJournal(todo)
+    todo_jrnl.add_entry("Бег 5 км")
+    todo_jrnl.add_entry("Тесты по питону")
+    count_of_entries = len(todo_jrnl)
+    assert expected_len == count_of_entries
